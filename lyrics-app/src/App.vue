51d50828adf6036fe6e7beb59e56bc15dfc572ca
Held Grijo <held.grijo@gmail.com>
2022-12-27 14:49:26 -0400

    feat: cria componente  exibição de texto
    a partir de dados de arquivo

--- lyrics-app/src/App.vue
+++ lyrics-app/src/App.vue
@@ -1,17 +1,16 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
+import Lyric, { LyricProps } from './components/Lyric.vue'
+import lyricData from './data/lyrics.json'
+
+const lyrics:LyricProps[] = lyricData;
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <Lyric v-for="lyric,index in lyrics"
+        :key="index"
+        :title="lyric.title"
+        :author="lyric.author"
+        :verses="lyric.verses" />
 </template>
 
 <style scoped>
---
+++ lyrics-app/src/components/Lyric.vue
@@ -0,0 +1,38 @@
+<script setup lang="ts">
+export interface LyricProps {
+  title: string
+  author?: string
+  verses?: Array<{text:string}>
+}
+
+const props = withDefaults(
+  defineProps<LyricProps>(),
+  {
+    title: "Not informed",
+    author: "Not informed"
+    //TODO: add default value for verses property
+  }
+)
+</script>
+
+<template>
+  <div>
+    <h2>{{ title }}</h2>
+    <cite>{{ author }}</cite>
+    <ul>
+      <li  v-for="line,index in verses" :key="line.text">{{ line.text }}</li>
+    </ul>
+  </div>
+</template>
+
+
+
+<style scoped>
+ul {
+  margin: 1.5em 0;
+}
+
+li {
+  list-style: none;
+}
+</style>
