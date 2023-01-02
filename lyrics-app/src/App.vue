fcaf6b93e3a63d0089fc2bccbf121c14d5ee5149
Held Grijo <held.grijo@manaus.am.gov.br>
2023-01-02 17:00:44 -0400

    feat: substitui componente de remoção de tags

--- lyrics-app/src/App.vue
+++ lyrics-app/src/App.vue
@@ -1,18 +1,23 @@
 <script setup lang="ts">
+import TagRipper from "./components/HtmlTagRemover.vue";
 import Lyric, { LyricProps } from './components/Lyric.vue'
 import lyricData from './data/lyrics.json'
 
-const lyrics:LyricProps[] = lyricData;
+const lyrics: LyricProps[] = lyricData;
+
 </script>
 
+
 <template>
-  <Lyric v-for="lyric,index in lyrics"
-        :key="index"
-        :title="lyric.title"
-        :author="lyric.author"
-        :url="lyric.url"
-        :verses="lyric.verses" />
+  <aside>
+    <TagRipper />
+  </aside>
+  <main>
+    <Lyric v-for="lyric, index in lyrics" :key="index" :title="lyric.title" :author="lyric.author" :url="lyric.url"
+      :verses="lyric.verses" />
+  </main>
 </template>
+
 
 <style scoped>
 .logo {
@@ -20,9 +25,11 @@ const lyrics:LyricProps[] = lyricData;
   padding: 1.5em;
   will-change: filter;
 }
+
 .logo:hover {
   filter: drop-shadow(0 0 2em #646cffaa);
 }
+
 .logo.vue:hover {
   filter: drop-shadow(0 0 2em #42b883aa);
 }
